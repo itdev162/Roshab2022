5f9ce384d925bf9fd66a15dbf7df65b3d59d3690
ShaeB56 <brister3@gmatc.matc.edu>
2021-12-06 20:55:05 -0600

    Completed Activity 5

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -5,6 +5,7 @@ namespace Persistence
     public class DataContext: DbContext
     {
         public DbSet<Value> Values {get; set;}
+        public DbSet<Post> Posts {get; set; }
 
         public DataContext (DbContextOptions options) : base(options)
         {
