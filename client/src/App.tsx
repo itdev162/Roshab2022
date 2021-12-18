f94521e1840a3bb2495ab5cb25b105367d84058c
ShaeB56 <brister3@gmatc.matc.edu>
2021-12-18 14:30:40 -0600

    Completed Activity-9

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import axios from 'axios';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import PostList  from './components/PostList/PostList';
 import Post from './components/Post/Post';
-import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
+
 
 
 class App extends React.Component
@@ -13,8 +13,9 @@ class App extends React.Component
     posts: [],
     post: null
   }
+
   componentDidMount(){
-    axios.get('http://localhost:5000/api/values')
+    axios.get('http://localhost:5000/api/posts')
     .then ((response)=>{
       this.setState({
         posts: response.data
@@ -24,6 +25,7 @@ class App extends React.Component
       console.error(`Error fetching data: ${error}`);
     })
   }
+
   viewPost = (post) => {
     console.log (`view ${post.title}`);
     this.setState({
@@ -31,6 +33,20 @@ class App extends React.Component
     });
   }
 
+  deletePost = post =>{
+    axios
+    .delete(`http://localhost:5000/api/posts/${post.id}`)
+    .then(response => {
+      const newPosts = this.state.posts.filter(p => p.id !== post.id);
+      this.setState({
+        posts: [...newPosts]
+      });
+    })
+    .catch(error => {
+      console.error(`Error deleting post: ${error}`);
+    });
+
+  };
   render() {
     const {posts, post} = this.state;
   
@@ -44,7 +60,9 @@ class App extends React.Component
         <main className = "App-content">
           <Switch>
             <Route exact path= "/">
-              <PostList posts = {posts} clickPost ={this.viewPost}/>
+              <PostList posts = {posts}
+                        clickPost ={this.viewPost}
+                        deletePost= {this.deletePost}/>
             </Route>
             <Route path ="/posts/:postId">
               <Post post = {post}/>
